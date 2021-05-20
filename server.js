79ee237f32b97ac2e8770e0c15d595f63bb0ea95
JohnWink <luisbpires@hotmail.com>
2021-05-20 15:07:57 +0100

    cors fix attempt 5

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 */
+app.use(cors());
+
 app.use(function(req, res, next) { 
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
