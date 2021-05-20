3539daa232bba62f66010b9e134fcd140b507943
JohnWink <luisbpires@hotmail.com>
2021-05-20 14:48:45 +0100

    another cors fix attempt

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require("./models/index.js");
 
 
 var app = express();
-
+/*
 var corsOptions = {
     origin: "*",
     methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -15,6 +15,26 @@ var corsOptions = {
 
 
 app.use(cors(corsOptions));
+*/
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:8888','https://eager-hugle-673cd6.netlify.app']);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
